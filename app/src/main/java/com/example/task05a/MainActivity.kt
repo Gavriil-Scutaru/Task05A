b9400ad81801c4e50c4d4a38ced94fed05f773b5
Gavriil-Scutaru <s5315415@bournemouth.co.uk>
2022-04-07 16:39:15 +0100

    Initial commit of all files

---
+++ app/src/main/java/com/example/task05a/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.task05a
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
