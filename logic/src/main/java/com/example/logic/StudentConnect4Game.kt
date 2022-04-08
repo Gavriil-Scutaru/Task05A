72fec73510650c1d770907fe6d1b2186b342c7af
Gavriil-Scutaru <s5315415@bournemouth.co.uk>
2022-04-08 18:01:09 +0100

    completed up to task 5.10 - implemented token for player

--- logic/src/main/java/com/example/logic/StudentConnect4Game.kt
+++ logic/src/main/java/com/example/logic/StudentConnect4Game.kt
@@ -8,11 +8,35 @@ class StudentConnect4Game (
 ): Connect4Game {
 
     override fun getToken(column: Int, row: Int): Int {
-        TODO("Not yet implemented")
+        return data[column][row]
     }
 
     override fun playToken(column: Int, player: Int): Boolean {
-        TODO("Not yet implemented")
+        if (player <= 0) {
+            throw IllegalArgumentException("Player numbers start with 1")
+        }
+
+        for (row in 0 until rows) {
+            // If we find an empty cell we set it with the token and return
+            // The return breaks our loop so it only gets set (maximum) once
+            if (data[column][row] == 0) {
+                data[column][row] = player
+                return true
+            }
+        }
+        return false      // illegal move
     }
+
+    /** Set up two-dimensional array of integer, of size columns x rows
+     * and fill it with zeroes.
+     */
+    private var data: Array<IntArray> = Array(columns) { IntArray(rows) { 0 } }
+
+    // Not specified in the interface - just to show that other things can be added
+    /**
+     * This determines the player who's turn it currently is.
+     */
+    var playerTurn: Int = 1
+        private set
 }
 
